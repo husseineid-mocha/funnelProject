dbbc1b7ba2a3eedc6b684aad19fb679f9173ba2c
husseineid-mocha <husseingeid@gmail.com>
2021-06-12 21:27:25 -0400

    added external api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require('node-fetch');
 
 const app = express();
 
@@ -6,7 +7,14 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/stats", async(req, res) => {
+    const response = await fetch('http://nestio.space/api/satellite/data');
+    const data = await response.json();
+    console.log(data.altitude)
+
+    res.send(data)
+})
+
 // Define a port and start listening for connections.
 const port = 3000;
-
 app.listen(port, () => console.log(`Listening on port ${port}...`));
