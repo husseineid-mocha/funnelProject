94e651f020f461482c4ea63c03d1419253b22bd4
husseineid-mocha <husseingeid@gmail.com>
2021-06-13 01:29:49 -0400

    working on times

--- index.js
+++ index.js
@@ -7,12 +7,47 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/stats", async(req, res) => {
-    const response = await fetch('http://nestio.space/api/satellite/data');
-    const data = await response.json();
-    console.log(data.altitude)
+let dataArray = [];
 
-    res.send(data)
+setInterval(() => {
+    updateData()
+}, 10 * 1000)
+updateData()
+
+function updateData() {
+    fetch('http://nestio.space/api/satellite/data')
+        .then(response => response.json())
+        .then(data => dataArray.push({ altitude: data.altitude, time: Date(data.last_updated) }))
+
+    // let now = new Date()
+    // now.setMinutes(now.getMinutes() - 5)
+    // now = new Date(now);
+
+    dataArray.filter(item => {
+        newDate = new Date(item.time)
+        newDate < (newDate.setMinutes(newDate.getMinutes() + 5))
+
+        console.log(newDate, newDate.setMinutes(newDate.getMinutes() + 5))
+    })
+    console.log(dataArray)
+}
+
+
+app.get("/stats", async(req, res) => {
+    const newArray = dataArray.map(obj => obj.altitude)
+
+    const minimum = Math.min(...newArray);
+    const maximum = Math.max(...newArray);
+    const average = newArray.reduce((sum, value) => {
+        return sum + value
+    }, 0) / newArray.length
+
+    const responseData = {
+        minimum,
+        maximum,
+        average
+    }
+    res.send(JSON.stringify(responseData));
 })
 
 // Define a port and start listening for connections.
