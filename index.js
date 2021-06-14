3becbacfbf64efb82ae6e22a2b37887fe207755b
husseineid-mocha <husseingeid@gmail.com>
2021-06-14 13:02:00 -0400

    formatting

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const fetch = require('node-fetch')
+const express = require('express');
+const fetch = require('node-fetch');
 
-const app = express()
+const app = express();
 
 // dataArray will store the values from the last 5 mins
-let dataArray = []
+let dataArray = [];
 
 // the interval that will fetch the data every 10 secs
 setInterval(() => {
-    updateData()
-}, 10 * 1000)
+    updateData();
+}, 10 * 1000);
 
 // initial load before the setInterval begins at second 10
-updateData()
+updateData();
 
 // function to retrieve the average of an array of integers
 function averageFunction(array) {
     const average = array.reduce((sum, value) => {
-        return sum + value
-    }, 0) / array.length
-    return average
+        return sum + value;
+    }, 0) / array.length;
+    return average;
 }
 
 function updateData() {
@@ -27,65 +27,65 @@ function updateData() {
         .then(response => response.json())
         .then(data => dataArray.push({ altitude: data.altitude, time: data.last_updated }))
         .then(() => {
-            const lastEntry = new Date(dataArray[dataArray.length - 1].time)
-                // filter the dataArray by values that are less than 5 minutes old
+            const lastEntry = new Date(dataArray[dataArray.length - 1].time);
+            // filter the dataArray by values that are less than 5 minutes old
             dataArray = dataArray.filter(item => {
-                const instanceEntry = new Date(item.time)
-                return lastEntry.getTime() - instanceEntry.getTime() < (300 * 1000)
+                const instanceEntry = new Date(item.time);
+                return lastEntry.getTime() - instanceEntry.getTime() < (300 * 1000);
             })
         })
 }
 
 app.get('/stats', async(req, res) => {
-    const altitudes = dataArray.map(obj => obj.altitude)
+    const altitudes = dataArray.map(obj => obj.altitude);
 
-    const minimum = Math.min(...altitudes)
-    const maximum = Math.max(...altitudes)
-    const average = averageFunction(altitudes)
+    const minimum = Math.min(...altitudes);
+    const maximum = Math.max(...altitudes);
+    const average = averageFunction(altitudes);
 
     const responseData = {
         minimum,
         maximum,
         average
     }
-    res.send(JSON.stringify(responseData))
+    res.send(JSON.stringify(responseData));
 })
 
 function getHealth(dataArray) {
-    const lastEntry = new Date(dataArray[dataArray.length - 1].time)
+    const lastEntry = new Date(dataArray[dataArray.length - 1].time);
 
     // the array of values for one minute before the current minute
     const twoMinutesBefore = dataArray.filter(item => {
-        const minuteTime = new Date(item.time)
-        const time = lastEntry.getTime() - minuteTime.getTime()
-        return time < 120 * 1000 && time >= 60 * 1000
+        const minuteTime = new Date(item.time);
+        const time = lastEntry.getTime() - minuteTime.getTime();
+        return time < 120 * 1000 && time >= 60 * 1000;
     })
 
     // the array of values for the most current minute
     const oneMinuteBefore = dataArray.filter(item => {
-        const minuteTime = new Date(item.time)
-        return lastEntry.getTime() - minuteTime.getTime() < 60 * 1000
+        const minuteTime = new Date(item.time);
+        return lastEntry.getTime() - minuteTime.getTime() < 60 * 1000;
     })
 
     // the averages for the last minute and last two minutes
-    const oneMinAverage = averageFunction(oneMinuteBefore.map(item => item.altitude))
-    const twoMinAverage = averageFunction(twoMinutesBefore.map(item => item.altitude))
+    const oneMinAverage = averageFunction(oneMinuteBefore.map(item => item.altitude));
+    const twoMinAverage = averageFunction(twoMinutesBefore.map(item => item.altitude));
 
     if (oneMinAverage < 160) {
-        return ('WARNING: RAPID ORBITAL DECAY IMMINENT')
+        return ('WARNING: RAPID ORBITAL DECAY IMMINENT');
     } else if (twoMinAverage < 160) {
-        return ('Sustained Low Earth Orbit Resumed')
+        return ('Sustained Low Earth Orbit Resumed');
     } else {
-        return ('Altitude is A-OK')
+        return ('Altitude is A-OK');
     }
 }
 
 app.get('/health', async(req, res) => {
-    res.send(getHealth(dataArray))
+    res.send(getHealth(dataArray));
 })
 
 // Define a port and start listening for connections.
-const port = 3000
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-module.exports = { getHealth: getHealth }
+module.exports = { getHealth: getHealth };
--- test/getHealth.test.js
+++ test/getHealth.test.js
@@ -1,12 +1,12 @@
-const assert = require('assert')
+const assert = require('assert');
 
-const { getHealth } = require('../index.js')
+const { getHealth } = require('../index.js');
 
-const startTime = new Date("2021-06-14T14:35:30")
+const startTime = new Date("2021-06-14T14:35:30");
 
 function generateTestData(altitudes) {
     return altitudes.map(altitude => {
-        startTime.setSeconds(startTime.getSeconds() + 10)
+        startTime.setSeconds(startTime.getSeconds() + 10);
         return {
             altitude,
             time: startTime.toISOString()
@@ -16,17 +16,17 @@ function generateTestData(altitudes) {
 
 describe('getHealth', function() {
     it('should return A-OK if average altitude is over 160km', function() {
-        const testData = generateTestData(Array.from({ length: 12 }, () => 178))
-        assert.equal(getHealth(testData), 'Altitude is A-OK')
-    })
+        const testData = generateTestData(Array.from({ length: 12 }, () => 178));
+        assert.equal(getHealth(testData), 'Altitude is A-OK');
+    });
 
-    it('should return WARNING if average altitude is less  160km', function() {
-        const testData = generateTestData(Array.from({ length: 12 }, () => 128))
-        assert.equal(getHealth(testData), 'WARNING: RAPID ORBITAL DECAY IMMINENT')
-    })
+    it('should return WARNING if average altitude is less than 160km', function() {
+        const testData = generateTestData(Array.from({ length: 12 }, () => 128));
+        assert.equal(getHealth(testData), 'WARNING: RAPID ORBITAL DECAY IMMINENT');
+    });
 
-    it('should return Sustained if average altitude is less  160km but now above', function() {
-        const testData = generateTestData([...Array.from({ length: 6 }, () => 128), ...Array.from({ length: 6 }, () => 187)])
-        assert.equal(getHealth(testData), 'Sustained Low Earth Orbit Resumed')
-    })
+    it('should return Sustained if average altitude was less than 160km but now above', function() {
+        const testData = generateTestData([...Array.from({ length: 6 }, () => 128), ...Array.from({ length: 6 }, () => 187)]);
+        assert.equal(getHealth(testData), 'Sustained Low Earth Orbit Resumed');
+    });
 })
