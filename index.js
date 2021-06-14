61cf21300f41c2af56e2fa5f2b6212ec6b674f5a
husseineid-mocha <husseingeid@gmail.com>
2021-06-13 20:03:49 -0400

    changes

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ function updateData() {
     if (dataArray[0]) {
         let instanceEntry = new Date(dataArray[0].time) // need to create instance of Date class for time manipulation
         let lastEntry = new Date(dataArray[dataArray.length - 1].time)
-            // console.log(instanceEntry.getTime(), lastEntry.getTime())
-            // console.log("SUBTRACT", lastEntry.getTime() - instanceEntry.getTime(), lastEntry.getTime() - instanceEntry.getTime() < 300000)
         if (lastEntry.getTime() - instanceEntry.getTime() > 300000) { // making sure the difference is less than 5 seconds
             dataArray.shift()
         }
@@ -47,8 +45,58 @@ app.get("/stats", async(req, res) => {
     res.send(JSON.stringify(responseData));
 })
 
+
 app.get('/health', async(req, res) => {
-    res.send('hello world')
+    let dataArrayHealth = dataArray.filter(item => {
+        let minuteTime = new Date(item.time)
+        let lastEntry = new Date(dataArray[dataArray.length - 1].time)
+        return lastEntry.getTime() - minuteTime.getTime() < 60000
+    })
+
+    const newArrayHealth = dataArrayHealth.map(obj => obj.altitude)
+
+    const average = newArrayHealth.reduce((sum, value) => {
+        return sum + value
+    }, 0) / newArrayHealth.length
+
+    let message
+    let timeoutFlag
+
+    function timer() {
+        message = 'Sustained Low Earth Orbit Resumed'
+        let count = 60
+        return timer = setInterval(() => {
+            console.log('hello')
+            count--
+            if (count <= 0) {
+                timeoutFlag = false
+                clearInterval(timer)
+            }
+            console.log(count)
+        }, 10000)
+    }
+
+    if (average < 160) {
+        message = 'WARNING: RAPID ORBITAL DECAY IMMINENT'
+        timeoutFlag = true
+        count = 60
+    } else {
+        timer()
+            // if (timeoutFlag) {
+            //     timer()
+            // } else {
+            //     message = 'Altitude is A-OK'
+            // }
+    }
+
+    let responseDataHealth = {
+        message: message,
+        average: average,
+        array: dataArrayHealth,
+        flag: timeoutFlag
+    }
+
+    res.send(JSON.stringify(responseDataHealth))
 })
 
 // Define a port and start listening for connections.
